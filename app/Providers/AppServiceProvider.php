4242377c0dfda0e59c65e8d3af15377d2c96d382
takuro.moto <fanky0jonson@gmail.com>
2021-03-31 12:16:23 +0000

    http

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,13 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') == 'production') {
-            \URL::forceScheme('http');
-        }
         
-        if (env('APP_ENV') != 'local') {
-            \URL::forceScheme('https');
-        }
+        // if (env('APP_ENV') != 'local') {
+        //     \URL::forceScheme('https');
+        // }
         
     }
 }
